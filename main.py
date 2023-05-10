67f862aa7c9fdeaa83b8bec7a11d8caed3558c89
IlyaKovalchukKIS <131522538+IlyaKovalchukKIS@users.noreply.github.com>
2023-05-10 09:35:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from random import choice
 
+
+def random_word(words: list):
+    result = choice(words)
+    return result
+    
+    
+def encode_word(word: str, morse: list):
+    for key, value in morse.items():
+        
